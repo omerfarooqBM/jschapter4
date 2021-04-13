16bfb0a108e7d39d3ed27c9de9ecc3bb01033129
omerfarooq <omer91999@gmail.com>
2021-04-13 12:59:41 +0500

    c2

--- app.js
+++ app.js
@@ -8,13 +8,14 @@
 // var my_2ndHabit;
 
 // // illegal variables
+//  var @address;
+//  var for;
 //  var 5name;
 //  var &age;
-//  var @address;
 //  var habit-1;
-//  var for;
+
 
  document.write("<h1>Rules for naming variables in JS</h1> \n variables name can only contain numbers,$ and _ For example <b> $my-1stVariable </b><br>");
  document.write("Variable nmust be begin with a letter,$ and _.For example $name,_nmae or name<br>");
  document.write("Variables are case sensitive<br>");
- document.write("Variables should not be JS keyword" + "<br>");
+ document.write("Variables should not be JS keyword");
